295b530af029e0a8048b7d6ac9f158fa0641c064
dasunNimantha <dasunnimantha77@outlook.com>
2021-05-29 23:07:18 +0530

    initialize commit

---
+++ components/Footer.js
@@ -0,0 +1,27 @@
+const Footer = ()=>{
+    return(
+        <footer class="site-footer clearfix">
+            <div class="footer-social">
+            <ul class="footer-social-links">
+                <li>
+                <a href="#" target="_blank">Twitter</a>
+                </li>
+
+                <li>
+                <a href="#" target="_blank">Facebook</a>
+                </li>
+
+                <li>
+                <a href="#" target="_blank">LinkedIn</a>
+                </li>
+            </ul>
+            </div>
+                
+            <div class="footer-copyrights">
+            <p>© 2021 All rights reserved.</p>
+            </div>
+      </footer>
+    );
+}
+
+export default Footer
